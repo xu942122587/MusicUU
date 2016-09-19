05566777445147872fe1ed63fa7895746fcff5b4
qtfreet00 <qtfreet00@52pojie.cn>
2016-09-19 16:53:25 +0800

    更新视频播放页面

--- app/src/main/java/com/qtfreet/musicuu/ui/activity/VideoActivity.java
+++ app/src/main/java/com/qtfreet/musicuu/ui/activity/VideoActivity.java
@@ -1,83 +1,41 @@
 package com.qtfreet.musicuu.ui.activity;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
 
+import com.dou361.ijkplayer.listener.OnPlayerBackListener;
+import com.dou361.ijkplayer.widget.PlayerView;
 import com.qtfreet.musicuu.R;
 import com.qtfreet.musicuu.model.Constant.Constants;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import tcking.github.com.giraffeplayer.GiraffePlayer;
-import tv.danmaku.ijk.media.player.IMediaPlayer;
-
 public class VideoActivity extends AppCompatActivity {
-
-    GiraffePlayer player;
-    @Bind(R.id.toolbar)
-    Toolbar toolbar;
-    @Bind(R.id.title_name)
-    TextView toolbarTitle;
-    private String url = "";
-    private String videoName;
+    private PlayerView player;
+    private View rootView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mv);
-        videoName = getIntent().getExtras().getString(Constants.NAME);
-        url = getIntent().getExtras().getString(Constants.URL);
-        init();
-        play();
+        rootView = getLayoutInflater().from(this).inflate(R.layout.simple_player_view_player, null);
+        setContentView(rootView);
+        String videoName = getIntent().getExtras().getString(Constants.NAME);
+        String url = getIntent().getExtras().getString(Constants.URL);
+        player = new PlayerView(this, rootView)
+                .setTitle(videoName)
+                .setOnlyFullScreen(true)
+                .forbidTouch(false)
+                .hideMenu(true)
+                .setPlaySource("默认", url).setShowSpeed(true)
+                .setPlayerBackListener(new OnPlayerBackListener() {
+                    @Override
+                    public void onPlayerBack() {
+                        //这里可以简单播放器点击返回键
+                        finish();
+                    }
+                })
+                .startPlay();
+
     }
-
-
-    private void play() {
-        player = new GiraffePlayer(this);
-        player.setFullScreenOnly(true);
-        player.onComplete(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), "video play completed", Toast.LENGTH_SHORT).show();
-            }
-        }).onInfo(new GiraffePlayer.OnInfoListener() {
-            @Override
-            public void onInfo(int what, int extra) {
-                switch (what) {
-                    case IMediaPlayer.MEDIA_INFO_BUFFERING_START:
-                        break;
-                    case IMediaPlayer.MEDIA_INFO_BUFFERING_END:
-                        break;
-
-                }
-            }
-        }).onError(new GiraffePlayer.OnErrorListener() {
-            @Override
-            public void onError(int what, int extra) {
-                Toast.makeText(getApplicationContext(), "video play error", Toast.LENGTH_SHORT).show();
-            }
-        });
-        player.play(url);
-    }
-
-    private void init() {
-        ButterKnife.bind(this);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-            if (toolbarTitle != null) {
-                getSupportActionBar().setDisplayShowTitleEnabled(false);
-                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-                toolbarTitle.setText(videoName);
-            }
-        }
-    }
-
 
     @Override
     protected void onPause() {
@@ -97,29 +55,17 @@ public class VideoActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-
         super.onDestroy();
         if (player != null) {
             player.onDestroy();
         }
     }
 
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (player != null) {
-            player.onConfigurationChanged(newConfig);
-        }
-    }
-
     @Override
     public void onBackPressed() {
-
         if (player != null && player.onBackPressed()) {
             return;
         }
-
         super.onBackPressed();
     }
 }
